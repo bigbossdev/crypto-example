e6b68eb97a32402330bb61ec41f49f36a5b2de0b
Big Boss <bigboss@bigboss.io>
2019-08-21 23:53:53 +0900

    Initial Commit

---
+++ CryptoExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CryptoExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Hello, World!";
+            string password = "MYPASSWORD";
+
+            string encrypted = PBEWithMD5AndDES.Encrypt(text, password);
+            Console.WriteLine("encrypted: " + encrypted);
+
+            string decrypted = PBEWithMD5AndDES.Decrypt(encrypted, password);
+            Console.WriteLine("decrypted: " + decrypted);
+        }
+    }
+}
